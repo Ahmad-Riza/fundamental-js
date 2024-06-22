9e339df798b388da5c3a819d7bfaf8aba438e1df
Ahmad-Riza <reeza.turkey25@gmail.com>
2024-06-23 01:36:49 +0700

    tipe data dalam javascript

---
+++ 4.data-types/data-types.js
@@ -0,0 +1,25 @@
+// string -> tipe data yang bersifat text
+const myName = `Theo`; // bisa pakai ``/""/''
+
+console.log(myName);
+
+// number ->  tipe data yang bersifat angka
+const age = 23;
+const carPrice = 200_000_000; // bisa pakai _ supaya bisa mudah di baca
+
+console.log(age); // untuk mengecek tipe data di console log pakai typeof "console.log(typeof age)"
+
+// boolean -> tipe data yang bersifat benar atau salah
+const isYoung = false;
+
+console.log(isYoung);
+
+// null -> tipe data yang bersifat kosong
+const myCar = null;
+
+console.log(myCar);
+
+// undefined -> tipe data yang bersifat tidak punya value atau tidak diketahui
+let myCompany;
+
+console.log(myCompany);
