b065921da2e70efa8583a464f47f6befd14a22fc
Ahmad-Riza <reeza.turkey25@gmail.com>
2024-06-23 02:53:51 +0700

    aritmetic dan comparison

---
+++ 5.operator/aritmetic.js
@@ -0,0 +1,53 @@
+// addition (+) -> penambahan dalam javascript
+// const x = 5;
+// const y = 3;
+// const result = x + y;
+
+// console.log(result);
+
+// subtraction (-) -> pengurangan dalam javascript
+// const x = 5;
+// const y = 3;
+// const result = x - y;
+
+// console.log(result);
+
+// multiplication (*) -> perkalian dalam javascript
+// const x = 5;
+// const y = 3;
+// const result = x * y;
+
+// console.log(result);
+
+// division (/) -> pembagian dalam javascript
+// const x = 5;
+// const y = 3;
+// const result = x / y;
+
+// console.log(result);
+
+// exponentiation (**) -> pangkat dalam javascript
+// const x = 5;
+// const y = 3;
+// const result = x ** y;
+
+// console.log(result);
+
+// modulus (%) -> sisa bagi dalam javascript
+const x = 150;
+const y = 3;
+const result = x % y;
+
+console.log(result);
+
+// increment (++) -> nambahin 1 dalam javascript
+// let x = 5;
+// x++;
+
+// console.log(x);
+
+// decrement (--) -> kurangin 1 dalam javascript
+// let y = 10;
+// x--; // decrement
+
+// console.log(y);
---
+++ 5.operator/comparison.js
@@ -0,0 +1,64 @@
+// equal to (==) -> ngecek isi value sama atau tidak
+// const x = 5;
+// const y = 5;
+// const result = x == y;
+
+// console.log(result);
+
+// strict/deep equal (===) -> ngecek isi value dan tipe data sama atau tidak
+// const x = "5";
+// const y = 5;
+// const result = x === y;
+
+// console.log(result);
+
+// not equal (!=) -> ngecek isi value beda atau tidak
+// const x = 5;
+// const y = 3;
+// const result = x != y;
+
+// console.log(result);
+
+// strict/deep not equal (!==) -> ngecek isi value dan tipe data beda atau tidak
+// const x = 5;
+// const y = 3;
+// const result = x !== y;
+
+// console.log(result);
+
+// greater than (>) -> ngecek lebih besar x dari y
+// const x = 5;
+// const y = 3;
+// const result = x > y;
+
+// console.log(result);
+
+// greater than equal (>=) -> ngecek lebih besar atau sama dengan x dari y
+// const x = 5;
+// const y = 3;
+// const result = x >= y;
+
+// console.log(result);
+
+// less than (<) -> ngecek lebih kecil x dari y
+// const x = 5;
+// const y = 3;
+// const result = x < y;
+
+// console.log(result);
+
+// less than equal (<=) -> ngecek lebih kecil atau sama dengan x dari y
+// const x = 5;
+// const y = 3;
+// const result = x <= y;
+
+// console.log(result);
+
+// LOGICAL OPERATORS
+// and (&&) -> semua harus true supaya hasil akhir jadi true
+const condition1 = true && true && false && false && true && true;
+
+// or (||) -> salah satu harus true, hasil akhir jadi true
+const condition2 = true || true || true || false || true || true;
+
+console.log(condition1, condition2);
