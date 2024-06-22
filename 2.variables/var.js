f950d41473492e64cdc47b199dfa51c7db603da8
Ahmad-Riza <reeza.turkey25@gmail.com>
2024-06-23 00:20:45 +0700

    menambahkan variables

---
+++ 2.variables/var.js
@@ -0,0 +1,13 @@
+var lunch = "Ayam Goreng"; //
+var myLunch = "Ayam Goreng"; // camel case (lebih dari 1 kata)
+var my_breakfast = "Ayam Goreng"; // snake case (katanya pakai underscore)
+var MyDinner = "Ayam Goreng"; // pascal case
+var MY_SNACK = "kacang"; // all caps
+
+console.log(myLunch);
+
+myLunch = "Telor Dadar";
+
+var x = 3;
+
+console.log(myLunch);
