2551d3e035ed5e024bf979f0f4be78cc10f93a60
Ahmad-Riza <reeza.turkey25@gmail.com>
2024-06-23 00:43:02 +0700

    perbedaan var,let dan const

---
+++ 3.scoped-variables/scoped.js
@@ -0,0 +1,10 @@
+let myLunch = "Ayam Goreng"; // let boleh ganti value, tapi gak boleh redeclare/dibuat ulang
+
+myLunch = "Sop Buntut";
+
+const myName = "Reza";
+
+// myName = "andi"; // kalau untuk const tidak boleh di ganti/tetap nilainya
+
+// console.log(myLunch);
+console.log(myName);
